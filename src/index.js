22f59ec2b7eb4cfaba947c035198d26c6af75cc6
bb <sm841004@gmail.com>
2019-06-01 14:37:30 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {combineReducers, createStore} from "redux";
+
+const productReducer = (state = [], action) => {
+  return state;
+}
+
+const userReducer = (state = '', action) => {
+  if (action.type === 'updateUser') {
+    return action.payload;
+  }
+  return state;
+}
+
+const allReducers = combineReducers({
+  product : productReducer,
+  user : userReducer
+})
+
+// 초기화 과정
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// 1. subscribe, 3: 데이터 수신
+store.subscribe(() => console.log('subscriber: ', store.getState()));
+
+// 2. publish: action dispatch
+const action = {
+  type: 'updateUser',
+  payload: {
+    newState: 'Tom'
+  }
+}
+store.dispatch(action);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
